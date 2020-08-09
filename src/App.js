24361aceef7bdacf6ab2e80be2bf542c2e9304ef
Messolhi said <messolhi_stic@hotmail.fr>
2020-08-09 15:42:31 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import uuid from 'uuid';
+import "bootstrap/dist/css/bootstrap.min.css";
+import TodoInput from './components/TodoInput';
+
+import TodoList from './components/TodoList';
+//showing vs-code github setup ------
+// dfsdfsdfsdf
+
+function App() {
+  return (
+    <div>
+   
+    <div className="container">
+      <div className="row">
+       <TodoInput />
+       <TodoList />       
+
+     </div>
+   </div>
+   </div>
+  );
+}
+
+export default App;
---
+++ src/components/TodoList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Item from "./TodoItem";
+
+
+export default class TodoList extends Component {
+    render() {
+        return (
+            <section>
+                <h2>to do list</h2>
+                <Item />
+            </section>
+        )
+    }
+}
