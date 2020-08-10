6073213a466deead223ecd9fc308a7d1e3e9654a
Messolhi said <messolhi_stic@hotmail.fr>
2020-08-10 11:38:01 +0100

    component structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import React from 'react';
-import uuid from 'uuid';
+import React, { Component } from 'react'
+import {v4 as uuid} from 'uuid';
 import "bootstrap/dist/css/bootstrap.min.css";
 import TodoInput from './components/TodoInput';
 
 import TodoList from './components/TodoList';
-//showing vs-code github setup ------
-// dfsdfsdfsdf
+class App extends Component {
 
-function App() {
-  return (
-    <div>
+  state = {
+    items: [{id:1, title:"wake up"},
+            {id:2,title:"takeBreak"}],
+    id: uuid(),
+    item: '',
+    editItem: false
+
+  };
+  handelChange =(e)=>{console.log('handle change')}
+  handelSubmit =(e)=>{console.log("handle Submit")}
+  clearList =()=>{console.log(`clear list`)}
+  handleDelete =(id)=>{console.log(`handle delete ${id}`)}
+  handleEdit =(id)=>{console.log(`handle edit ${id}`)}
+
+  render() {
+    return (
+      <div>
    
     <div className="container">
       <div className="row">
-       <TodoInput />
-       <TodoList />       
+        <div className="col-10 mx-auto col-md-8 mt-5">
+
+          <h3 className="text-capitalize text-center"> todo input</h3>
+
+        </div>
+       <TodoInput 
+       item={this.state.item} 
+       handleChange={this.handleChange} 
+       handleSubmit={this.handleSubmit} 
+       editItem={this.state.editItem}
+       
+       />
+       <TodoList 
+       items={this.state.items}
+       clearList={this.clearList}
+       handleDelete={this.handleDelete}
+       handleEdit = {this.handleEdit}
+       
+       />       
 
      </div>
    </div>
    </div>
-  );
+    )
+  }
 }
-
 export default App;
